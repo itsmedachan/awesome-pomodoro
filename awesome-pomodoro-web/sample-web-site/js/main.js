9a243a47682d50b3ce2791d65d41eb079eec4a07
itsmedachan <y.ishida1012@gmail.com>
2020-03-11 20:09:18 +0900

    sample like button

--- awesome-pomodoro-web/sample-web-site/js/main.js
+++ awesome-pomodoro-web/sample-web-site/js/main.js
@@ -1,10 +1,31 @@
 'use strict';
 
-{
-  let nowTime = new Date();
-  let nowHour = nowTime.getHours();
-  let nowMin  = nowTime.getMinutes();
-  let nowSec  = nowTime.getSeconds();
-  let time_title = nowHour + ":" + nowMin + ":" + nowSec;
-  document.title = time_title;
+let nowTime = new Date();
+let nowHour = nowTime.getHours();
+let nowMin  = nowTime.getMinutes();
+let nowSec  = nowTime.getSeconds();
+let time_title = nowHour + ":" + nowMin + ":" + nowSec;
+document.title = time_title;
+
+const e = React.createElement;
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
 }
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
